402094bef4919f2ddf45318b30a935f7ad6222b2
berger <robertberger.dev@gmail.com>
2023-03-02 14:25:30 -0500

    added toggle to navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navigation from './Navigation'
 import ThemeContext from './ThemeContext'
 
 function App() {
+  let [theme, setTheme] = useState({
+    variant: 'dark',
+    toggleTheme: toggleTheme
+  })
+
+  function toggleTheme() {
+    setTheme(theme => (
+      {
+        ...theme,
+        variant: theme.variant === 'dark' ? 'light' : 'dark',
+      }
+    ))
+  }
+
   return (
-    <ThemeContext.Provider value={{variant: 'dark'}}>
+    <ThemeContext.Provider value={{theme}}>
       <Navigation />
     </ThemeContext.Provider>
   );
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,27 @@
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import Container from 'react-bootstrap/Container'
+import ThemeContext from "./ThemeContext"
 
 function Navigation(props) {
-  return (
-    <Navbar bg="light" variant="light">
-      <Container>
-        <Navbar.Brand href="#home">Context Example App</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">About Us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
-}
+    return (
+      <ThemeContext.Consumer>
+      {({variant}) => (
+        <Navbar bg={variant} variant={variant}>
+          <Container>
+            <Navbar.Brand href="#home">Context Example App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#">Home</Nav.Link>
+                <Nav.Link href="#">About Us</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      )}
+      </ThemeContext.Consumer>
+    )
+  }
 
 export default Navigation
